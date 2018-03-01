4af9feda1935c3487458594d1e52d2d07efc0b12
ckckckc <refala246@gmail.com>
2018-03-01 20:52:57 +0800

    7.2 Reading and Writing Array Elements

--- Ch7 陣列/test.js
+++ Ch7 陣列/test.js
@@ -12,3 +12,32 @@ console.log('arr2 length:', arr2.length)
 var arr3 = [,,];
 console.log('arr3 length:', arr3.length)
 
+var arr4 = [];
+
+arr4[0] = 2;
+arr4[1.000] = 3;
+// arr4[-1] = 5;
+// arr4['2'] = 4;
+// arr4['4.000'] = 4;
+
+arr4.pro1 = 'str1';
+console.log(arr4)
+console.log('arr4 length:',arr4.length)
+
+var obj = {};
+obj[1] = "one";
+console.log('obj: ',obj)
+console.log('obj.length: ', obj.length)
+
+var arr5 = [];
+console.log('arr5.length:', arr5.length);
+arr5[maxArrayLength] = 'test';
+console.log('arr5[maxArrayLength + 1]:', arr5[maxArrayLength]); // convert to property
+console.log('arr5.length:', arr5.length);
+
+var arr6 = [];
+arr6[-1] = 'test1';
+arr6['2'] = 'test2';
+arr6[3.00] = 'test3';
+console.log('arr6:', arr6)
+console.log('arr6.length:', arr6.length)
