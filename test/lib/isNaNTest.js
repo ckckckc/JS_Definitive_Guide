d5a3fbd981a204b4450bdd98e9d939146499db42
ckckckc <refala246@gmail.com>
2018-04-12 19:01:54 +0800

    npm init, eslint, mocha, chai, isNaNTest.js

---
+++ test/lib/isNaNTest.js
@@ -0,0 +1,40 @@
+const assert = require('chai').assert;
+const isNaN = require('./../../src/lib/isNaN.js');
+
+describe('isNaN lib', () => {
+  it('should return true, while isNaN(NaN)', () => {
+    assert.equal(isNaN(NaN), true);
+  });
+
+  it('should return false, while isNaN(0)', () => {
+    assert.equal(isNaN(0), false);
+  });
+
+  it('should return false, while isNaN(-0)', () => {
+    assert.equal(isNaN(-0), false);
+  });
+
+  it('should return false, while isNaN("")', () => {
+    assert.equal(isNaN(''), false);
+  });
+
+  it('should return false, while isNaN(null)', () => {
+    assert.equal(isNaN(null), false);
+  });
+
+  it('should return false, while isNaN(undefined)', () => {
+    assert.equal(isNaN(undefined), false);
+  });
+
+  it('should return false, while isNaN()', () => {
+    assert.equal(isNaN(), false);
+  });
+
+  it('should return false, while isNaN(false)', () => {
+    assert.equal(isNaN(false), false);
+  });
+
+  it('should return false, while isNaN(Infinity)', () => {
+    assert.equal(isNaN(Infinity), false);
+  });
+});
