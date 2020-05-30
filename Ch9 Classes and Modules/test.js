a090f337e5defc149c67bacbf875db89e246eed4
ckckckc <refala246@gmail.com>
2020-05-30 16:47:44 +0800

    update Set prototype method test

--- Ch9 Classes and Modules/test.js
+++ Ch9 Classes and Modules/test.js
@@ -652,3 +652,135 @@ console.log(roi.readOnlyMethod());
 })();
 
 console.log('roi.objectId', roi.objectId)
+
+var Set = (function invocation() {
+  function Set() {
+    this.values = {};
+    this.n = 0;
+
+    this.add.apply(this, arguments);
+  }
+
+  Set.fromArray = function(array) {
+    var set = new Set();
+    set.add.apply(set, array);
+    return set;
+  };
+
+  function v2s(val) {
+    /* */
+  }
+
+  function objectId(o){
+    /* */
+  }
+
+  nextId = 1;
+
+  Set.prototype.toString = function() {
+    var s = '{', i = 0;
+
+    this.foreach(function(value){
+      s += ((i++ > 0) ? ', ' : '') + value;
+    });
+
+    return s + '}';
+  };
+
+  Set.prototype.toLocaleString = function() {
+    var s = '{', i = 0;
+
+    this.foreach(function(value){
+      if (i++ > 0) {
+        s += ', ';
+      }
+      // null & undefined
+      if (value == null) {
+        s += value;
+      }
+      // all others
+      else {
+        s += value.toLocaleString();
+      }
+    });
+
+    return s + '}';
+  };
+
+  // Convert a set to an array of values
+  Set.prototype.toArray = function() {
+    var array = [];
+
+    this.foreach(function(value) {
+      array.push(value);
+    });
+
+    return array;
+  };
+
+  // Treat sets like arrays for the purposes of JSON stringification.
+  Set.prototype.toJSON = Set.prototype.toArray;
+
+  Set.prototype.add = function() {
+    for (var i = 0, len = arguments.length ; i < len ; i++) {
+      var val = arguments[i];
+      var str = v2s(val);
+      if (!this.values.hasOwnProperty(str)) {
+        this.values[str] = val;
+        this.n++;
+      }
+    }
+    return this;
+  };
+
+  Set.prototype.remove = function() {
+    for (var i = 0, len = arguments.length ; i < len ; i++) {
+      var str = v2s(arguments[i]);
+      if (this.values.hasOwnProperty(str)) {
+        delete this.values[str];
+        this.n--;
+      }
+    }
+    return this;
+  };
+
+  Set.prototype.contains = function(value) {
+    return this.values.hasOwnProperty(v2s(value));
+  };
+
+  Set.prototype.size = function() {
+    return this.n;
+  };
+
+  Set.prototype.foreach = function(f, context) {
+    for (var s in this.values) {
+      // 忽略繼承而來的 properties
+      if (this.values.hasOwnProperty(s)) {
+        f.call(context, this.values[s]);
+      }
+    }
+  };
+
+  Set.prototype.equals = function(that) {
+    if (this === that) return true;
+
+    if (!(that instanceof Set)) return false;
+
+    if (this.size() != that.size()) return false;
+
+    try {
+      this.foreach(function(value) {
+        if (!that.contains(value)) {
+          throw false;
+        }
+        return true;
+      });
+    } catch (x) {
+      if (x === false) {
+        return false;
+      }
+
+      throw x;
+    }
+  };
+})();
